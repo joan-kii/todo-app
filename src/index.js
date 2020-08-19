a7df57d997d279422323f383e1d9df4cc937faeb
joan-kii <joan.sb81@gmail.com>
2020-08-19 19:02:54 +0200

    añade transform burger y plus. topBar finalizada

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
+// Inputs
+
+const burgerButton = document.getElementById('burgerIcon');
+console.log(burgerButton)
+burgerButton.addEventListener('click', function() {
+    burgerButton.classList.toggle('change');
+});
+const plusButton = document.getElementById('newTaskPlus');
+console.log(plusButton)
+plusButton.addEventListener('click', function() {
+    plusButton.classList.toggle('changeToCross');
+});
 const content = document.getElementById('content');
-content.innerText = 'lol';
+content.innerText = '';
