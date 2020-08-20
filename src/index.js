02e11700d69f6aebfd1eddb85cc6bf3fd26297a4
joan-kii <joan.sb81@gmail.com>
2020-08-20 06:03:31 +0200

    añade fuente

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 // Inputs
 
 const burgerButton = document.getElementById('burgerIcon');
-console.log(burgerButton)
 burgerButton.addEventListener('click', function() {
     burgerButton.classList.toggle('change');
 });
 const plusButton = document.getElementById('newTaskPlus');
-console.log(plusButton)
-plusButton.addEventListener('click', function() {
+const newTaskButton = document.getElementById('newTaskIcon');
+newTaskButton.addEventListener('click', function() {
     plusButton.classList.toggle('changeToCross');
 });
 const content = document.getElementById('content');
