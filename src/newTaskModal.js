e3a176d691f5e547e45a1e6dfeb28f3bc56c83b4
joan-kii <joan.sb81@gmail.com>
2020-08-25 05:59:39 +0200

    añade modal nueva tarea

--- src/newTaskModal.js
+++ src/newTaskModal.js
@@ -1,28 +1,33 @@
 // Function
 
 function newTaskModal(project) {
-    const modal = document.createElement('div');
-    modal.id = 'modal';
-    modal.className = 'modal';
+    // Modal
+    const newTaskModal = document.createElement('div');
+    newTaskModal.id = 'NewTaskModal';
+    newTaskModal.className = 'newTaskModal';
 
+    // Project Title
     const projectSelected = document.createElement('h2');
     projectSelected.id = 'projectSelected';
     projectSelected.className = 'projectSelected';
     projectSelected.textContent = project;
-    modal.appendChild(projectSelected);
+    newTaskModal.appendChild(projectSelected);
 
+    // New Task
     const modalTitle = document.createElement('h3');
     modalTitle.className = 'modalTitle';
     modalTitle.textContent = 'Nueva Tarea';
-    modal.appendChild(modalTitle);
+    newTaskModal.appendChild(modalTitle);
 
+    // Form
     const modalForm = document.createElement('form');
     modalForm.id = 'modalForm';
     modalForm.className = 'modalForm';
-    modal.appendChild(modalForm); 
+    newTaskModal.appendChild(modalForm); 
     // Posible bug. Añade modalForm a modal antes añadir
     // elementos a modalForm
     
+    // Task Title Input
     const taskTitle = document.createElement('input');
     taskTitle.id = 'taskTitle';
     taskTitle.className = 'taskTitle';
@@ -31,11 +36,67 @@ function newTaskModal(project) {
     taskTitle.placeholder = 'Título';
     modalForm.appendChild(taskTitle);
 
-    // create textarea
+    // Notes Input
+    const notes = document.createElement('textarea');
+    notes.id = 'notes';
+    notes.className = 'notes';
+    notes.placeholder = 'Descripción';
+    modalForm.appendChild(notes);
 
-    // create dueDate
+    // Due Date Input
+    const dueDateLabel = document.createElement('label');
+    dueDateLabel.htmlFor = 'dueDate';
+    const dueDate = document.createElement('input');
+    dueDate.id = 'dueDate';
+    dueDate.className = 'dueDate';
+    dueDate.type = 'date';
+    modalForm.appendChild(dueDate);
 
-    // create priority
+    // Priority Input
+    /* const priority = document.createElement('select');
+    priority.id = 'priority';
+    priority.className = 'priority';
 
-    // create button submit
-}
+    for (let i=0; i < 3; i++) {
+        let option = document.createElement('option');
+        option.value = i;
+        priority.options.add(option);
+    }; */
+
+    const priorityButtons = document.createElement('div');
+    priorityButtons.id = 'priorityButtons';
+    priorityButtons.className = ' priorityButtons';
+
+    const labelPrioritybuttons = document.createElement('label');
+    labelPrioritybuttons.htmlFor = 'priorityButtons';
+
+    const lowPriority = document.createElement('span');
+    lowPriority.id = 'lowPriority';
+    lowPriority.className = 'lowPriority';
+    lowPriority.value = '1';
+    priorityButtons.appendChild(lowPriority);
+
+    const mediumPriority = document.createElement('span');
+    mediumPriority.id = 'mediumPriority';
+    mediumPriority.className = 'mediumPriority';
+    lowPriority.value = '2';
+    priorityButtons.appendChild(mediumPriority);
+
+    const highPriority = document.createElement('span');
+    highPriority.id = 'highPriority';
+    highPriority.className = 'highPriority';
+    lowPriority.value = '3';
+    priorityButtons.appendChild(highPriority);
+
+    modalForm.appendChild(priorityButtons);
+
+    // Submit Button
+    const submitTask = document.createElement('input');
+    submitTask.id = 'submitTask';
+    submitTask.className = 'submitTask';
+    submitTask.type = 'submit';
+    submitTask.value = 'Crear Tarea';
+    modalForm.appendChild(submitTask);
+};
+
+export { newTaskModal };
