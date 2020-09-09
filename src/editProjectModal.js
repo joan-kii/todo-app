2dc88d72af53a7101e0909af80d7e2e1f3171aeb
joan-kii <joan.sb81@gmail.com>
2020-09-09 05:55:58 +0200

    añade primeros estilos 'editProjectModal'

--- src/editProjectModal.js
+++ src/editProjectModal.js
@@ -6,19 +6,24 @@ const editProjectModalRender = () => {
     editProjectModal.id = 'editProjectModal';
     editProjectModal.className = 'editProjectModal';
 
+    const popEditProject = document.createElement('div');
+    popEditProject.id = 'popEditProject';
+    popEditProject.className = 'popEditProject';
+    editProjectModal.appendChild(popEditProject);
+
     const cancelEditButton = document.createElement('button');
     cancelEditButton.id = 'cancelEditButton';
     cancelEditButton.className = 'cancelEditButton';
     cancelEditButton.type = 'button';
     cancelEditButton.textContent = 'Cancelar';
-    editProjectModal.appendChild(cancelEditButton);
+    popEditProject.appendChild(cancelEditButton);
 
     const deleteProjectButton = document.createElement('button');
     deleteProjectButton.id = 'deleteProjectButton';
-    deleteProjectButton.className = 'deleteprojectButton';
+    deleteProjectButton.className = 'deleteProjectButton';
     deleteProjectButton.type = 'button';
     deleteProjectButton.textContent = 'Eliminar Proyecto';
-    editProjectModal.appendChild(deleteProjectButton);
+    popEditProject.appendChild(deleteProjectButton);
     
     // Secondary Functions
     
