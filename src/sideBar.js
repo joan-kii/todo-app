507e72d073768aec125cece4f37e6eff35b3743d
joan-kii <joan.sb81@gmail.com>
2020-09-11 18:22:49 +0200

    añade estilos 'taskListItem'

--- src/sideBar.js
+++ src/sideBar.js
@@ -7,11 +7,9 @@ const sideBar = document.getElementById('sideBar');
 const createSideBar = () => {
     if (sideBar.style.width === '25%') {
         sideBar.style.width = '0';
-        content.style.marginLeft = '0';
 
     } else {
         sideBar.style.width = '25%';
-        content.style.marginLeft = '25%'; 
     };
 };
 
